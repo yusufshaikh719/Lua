b040b97c16045cd57f516608d91013f997f86ad6
Yusuf Shaikh <61252533+yusufshaikh719@users.noreply.github.com>
2023-01-14 11:44:13 -0800

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,8 @@
+print("Hellow and welcome to 'THE DARK SUNRISE'\nCommands: explore\tspeak: (ex. speak: Hello)")
+print("You wake up to the sound of crickets and insects in the air, with no memory of how you got there")
+print("Taking up the wooden sword you find in your hand, you stand")
+local wakingUp = io.read()
+
+if wakingUp == "explore" then
+    print("")
+end
